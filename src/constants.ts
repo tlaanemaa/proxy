b0a4223b3477b1dc6978d99089e7c6161bde72f6
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2024-02-28 21:37:37 +0100

    Improved comments

--- src/constants.ts
+++ src/constants.ts
@@ -1,19 +1,19 @@
 /**
- * Default target header to be sent to the server.
+ * Target header to be sent to the server.
  * This can be customized from environment variables.
  */
 export const TARGET_HEADER: string =
   process.env.TARGET_HEADER_NAME ?? "x-target-host";
 
 /**
- * Default target query param to be sent to the server.
+ * Target query param to be sent to the server.
  * This can be customized from environment variables.
  */
 export const TARGET_QUERY_PARAM: string =
   process.env.TARGET_QUERY_PARAM ?? "target";
 
 /**
- * Default server port
+ * Server port
  * This can be customized from environment variables
  */
 export const PORT: number = parseInt(process.env.PORT!) || 3000;
